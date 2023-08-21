25f97b336e17e638ac702004795394d761cc3479
zan-118 <fauzanabdulbasith@gmail.com>
2023-08-21 21:22:27 +0700

    Initial Commit

---
+++ src/components/banner/LeftBanner.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Media from "./Media";
+
+const LeftBanner = () => {
+  const [text] = useTypewriter({
+    words: ["Full Stack Developer.", "Data Analyst."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm{" "}
+          <span className="text-designColor capitalize">
+            Fauzan Abdul Basith
+          </span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+          Crafting immersive web experiences and dissecting data trends. As a
+          freelance web developer, I transform ideas into user-friendly
+          applications. Meanwhile, my data analyst role allows me to unravel
+          insights that drive informed decisions.
+        </p>
+      </div>
+      {/* Media */}
+      <Media />
+    </div>
+  );
+};
+
+export default LeftBanner;
---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Front-End",
+    des: "HTML, CSS, JavaScript,Tailwind, Bootstrap, ReactJS",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "Back-End",
+    des: "Node.js, Express.js, RestFul API",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "Framework",
+    des: "Next.js",
+  },
+  {
+    id: 4,
+    icon: <FaMobile />,
+    title: "Database",
+    des: "PostgreSQL, BigQuery",
+  },
+  {
+    id: 5,
+    icon: <SiAntdesign />,
+    title: "Data Analysis",
+    des: "Rstudio, Spreadsheet, Data Visualization",
+  },
+  {
+    id: 6,
+    icon: <FaGlobe />,
+    title: "Hosting Websites",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+];
