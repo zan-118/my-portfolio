5c5d82929790f50faf50d32d633764a2be9ec626
zan-118 <fauzanabdulbasith@gmail.com>
2023-09-01 01:06:20 +0700

    update ProjectCard link to code and live

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,8 @@
 import logo from "./images/zan-logo (2).png";
 import bannerImg from "./images/bannerImg.png";
-import projectOne from "./images/projects/projectOne.jpg";
-import projectTwo from "./images/projects/projectTwo.jpg";
-import projectThree from "./images/projects/projectThree.jpeg";
+import projectOne from "./images/projects/projectOne.png";
+import projectTwo from "./images/projects/projectTwo.png";
+import projectThree from "./images/projects/projectOne.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Title from '../layouts/Title'
+import React from "react";
+import Title from "../layouts/Title";
 import { projectOne, projectTwo, projectThree } from "../../assets/index";
-import ProjectsCard from './ProjectsCard';
+import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
   return (
@@ -17,16 +17,18 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Gaming Website"
+          des=" A Final Project when I took Full Stack Web at Binar Academy about gaming website which is developed by a Alfa Centaury Team."
           src={projectOne}
+          sourceCode="https://gitlab.com/fauzan118/challenge-11-team-1-fe"
+          websiteLink="https://challenge-11-team-1-fe.vercel.app/"
         />
         <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Bike Sharing Analysis"
+          des=" This analytics case study is related to the Capstone project requirements for Google Data Analytics Professional Certificate. The case study contains data for a bike sharing company oon customer trip details over a 12-month period (July 2021 to June 2022). "
           src={projectTwo}
+          sourceCode="https://www.kaggle.com/code/fauzanabdulbasith/case-study-bike-sharing-for-dts-proa"
+          websiteLink="https://zan-118.github.io/portofolio/"
         />
         <ProjectsCard
           title="Chatting App"
@@ -41,20 +43,22 @@ const Projects = () => {
           src={projectThree}
         />
         <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Gaming Website"
+          des=" A Final Project when I took Full Stack Web at Binar Academy about gaming website which is developed by a Alfa Centaury Team."
           src={projectOne}
+          sourceCode="https://gitlab.com/fauzan118/challenge-11-team-1-fe"
+          websiteLink="https://challenge-11-team-1-fe.vercel.app/"
         />
         <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Bike Sharing Analysis"
+          des=" This analytics case study is related to the Capstone project requirements for Google Data Analytics Professional Certificate. The case study contains data for a bike sharing company oon customer trip details over a 12-month period (July 2021 to June 2022). "
           src={projectTwo}
+          sourceCode="https://www.kaggle.com/code/fauzanabdulbasith/case-study-bike-sharing-for-dts-proa"
+          websiteLink="https://zan-118.github.io/portofolio/"
         />
       </div>
     </section>
   );
-}
+};
 
-export default Projects
+export default Projects;
