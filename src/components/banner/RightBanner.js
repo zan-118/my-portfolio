2ec2b3ab05ddbe472ea01df454e6c00864b08f90
zan-118 <fauzanabdulbasith@gmail.com>
2023-09-28 22:20:38 +0700

    remove bg image

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,7 +5,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+        className="w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] z-10"
         src={bannerImg}
         alt="bannerImg"
       />
